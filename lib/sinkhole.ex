4e05dbe6f07cc3623b3813a6df1dcc2a4b8dca40
oldironhorse <s1m0n.r3dd1ng@gmail.com>
2018-02-21 11:56:28 +0000

    domain added as argument to start_link

--- lib/sinkhole.ex
+++ lib/sinkhole.ex
@@ -1,22 +1,23 @@
 defmodule Sinkhole do
   use GenMQTT
+
   @moduledoc """
   Documentation for Sinkhole.
   """
 
-  def start_link(topic) do
-    GenMQTT.start_link(__MODULE__, [topic], [{:host, "euterpe3"}])
+  def start_link(domain) do
+    GenMQTT.start_link(__MODULE__, domain, [{:host, "euterpe3"}])
   end
 
-  def on_connect(state) do
-    IO.puts "on_connect(#{state}): Connected. Subscribing..."
-    :ok = GenMQTT.subscribe(self(), "room/+/temp", 0)
-    {:ok, state}
+  def on_connect(domain) do
+    IO.puts("on_connect(#{domain}): Connected. Subscribing...")
+    :ok = GenMQTT.subscribe(self(), "#{domain}/#", 0)
+    {:ok, domain}
   end
 
-  def on_publish(["room", location, "temp"], message, state) do
-    IO.puts "It is #{message} degrees in #{location}"
-    {:ok, state}
+  def on_publish([domain | location], message, domain) do
+    IO.puts("It is #{message} degrees in #{location}")
+    {:ok, domain}
   end
 
   @doc """
--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule Sinkhole.MixProject do
   defp deps do
     [
       {:gen_mqtt, "~> 0.4.0"},
-      {:mongodb, "~> 0.4.4"},
+      {:mongodb, "~> 0.4.4"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]

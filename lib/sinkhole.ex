7b8abf0d0bb3a025d0a8489cacd12506ecc66bc5
oldironhorse <s1m0n.r3dd1ng@gmail.com>
2018-02-21 11:26:19 +0000

    basic MQTT subscription working

--- lib/sinkhole.ex
+++ lib/sinkhole.ex
@@ -1,7 +1,23 @@
 defmodule Sinkhole do
+  use GenMQTT
   @moduledoc """
   Documentation for Sinkhole.
   """
+
+  def start_link(topic) do
+    GenMQTT.start_link(__MODULE__, [topic], [{:host, "euterpe3"}])
+  end
+
+  def on_connect(state) do
+    IO.puts "on_connect(#{state}): Connected. Subscribing..."
+    :ok = GenMQTT.subscribe(self(), "room/+/temp", 0)
+    {:ok, state}
+  end
+
+  def on_publish(["room", location, "temp"], message, state) do
+    IO.puts "It is #{message} degrees in #{location}"
+    {:ok, state}
+  end
 
   @doc """
   Hello world.
